baf6c109f092655930464fd2084043b5f525fced
Hans Christian Rebling <hans.chr.rebling@gmail.com>
2024-12-05 13:58:23 +0100

    60 fps ticks implemented

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 def main():
     # Initialize Pygame
     pygame.init()
+
+    # Create a clock object
+    clock = pygame.time.Clock()
+    dt = 0
     
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,6 +25,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 if __name__ == "__main__":
     main()
