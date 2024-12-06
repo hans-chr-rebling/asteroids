344bc71428d9847a28e0d67978a3a1b7ac694b11
Hans Christian Rebling <hans.chr.rebling@gmail.com>
2024-12-06 20:29:22 +0100

    move forward and backwards added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
 def main():
@@ -24,14 +24,15 @@ def main():
     # Main game loop
     running = True
     while running:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
         screen.fill((0, 0, 0))
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
         
 if __name__ == "__main__":
     main()
